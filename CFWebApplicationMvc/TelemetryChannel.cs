f701fa7a42f3da3cd51825c8af907e7bcfa4e68f
Shawn Neal <sneal@pivotal.io>
2018-06-13 09:43:50 -0400

    Add application insights CF telemetry channel

---
+++ CFWebApplicationMvc/TelemetryChannel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+namespace CFWebApplicationMvc
+{
+    public class TelemetryChannel : ITelemetryChannel
+    {
+        public TelemetryChannel()
+        {
+            Trace.WriteLine("TelemetryChannel ctor");
+        }
+
+        public bool? DeveloperMode
+        {
+            get { return true; }
+            set { }
+        }
+
+        public string EndpointAddress
+        {
+            get { return ""; }
+            set { }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void Flush()
+        {
+            
+        }
+
+        public void Send(ITelemetry item)
+        {
+            item.Sanitize();
+            var b = JsonSerializer.Serialize(new ITelemetry[] { item }, false);
+            var s = Encoding.UTF8.GetString(b);
+            Trace.WriteLine(string.Format("AI: {0}", s));
+        }
+    }
+}
